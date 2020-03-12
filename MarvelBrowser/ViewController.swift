06d48621481d5987e2c19afde2a9eb29338047b5
Michael Borgmann <michaelborgmann@mailbox.org>
2020-03-12 13:22:08 +0000

    Initial Commit

---
+++ MarvelBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelBrowser
+//
+//  Created by Michael Borgmann on 12.03.20.
+//  Copyright © 2020 Michael Borgmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
